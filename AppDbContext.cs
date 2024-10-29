715feb0303b812527beadb0a77c05dec0a478ccc
kamouniyoussef <youssefkamouni789@gmail.com>
2024-10-29 22:49:54 +0100

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+
+using gestion_hotel.Models;
+using Microsoft.EntityFrameworkCore;
+ 
+namespace gestion_hotel.Data
+{
+    public class AppDbContext : DbContext
+    { 
+       public AppDbContext(DbContextOptions<AppDbContext> options ):base(options) {
+ 
+
+
+        }
+        public DbSet<Chambre> Chambres { get; set; }
+    }
+}
---
+++ Chambre.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gestion_hotel.Models
+{
+    public class Chambre
+    {
+        [Key]
+        public int  Id {get; set;}
+        [Required]  
+        public required string Nom { get; set; }
+        public required string Prenom { get; set; }
+
+    }
+}
